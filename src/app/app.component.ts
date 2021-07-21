ad40606ea2240fbd0704572bdb2d521fca47be49
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-07-21 17:08:03 +0300

    Demo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ConfirmationService]
 })
 export class AppComponent {
-  title = 'primeng-i18n';
+
+  lang: string = "en";
+
+  date: any;
+
+  uploadedFiles: any[] = [];
+
+  password: string = "";
+
+  constructor(public translate: TranslateService, public primeNGConfig: PrimeNGConfig, private confirmationService: ConfirmationService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    let lang = browserLang.match(/en|fr/) ? browserLang : 'en';
+    this.changeLang(lang);
+  }
+
+  changeLang(lang: string) {
+    this.translate.getTranslation(lang).subscribe(value => {
+      this.translate.use(lang);
+      this.primeNGConfig.setTranslation(value.primeng)
+    })
+  }
+
+  onUpload(event: any) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+  }
+
+  confirm(event: any) {
+    this.confirmationService.confirm({
+      target: event.target,
+      message: this.translate.instant('demo.message'),
+      icon: 'pi pi-exclamation-triangle'
+    });
+  }
+
+  customers = [
+    { "name": "Yancey" },
+    { "name": "Chilton" },
+    { "name": "Angelo" },
+    { "name": "Carita" },
+    { "name": "Wernher" },
+    { "name": "Danella" },
+    { "name": "Harli" },
+    { "name": "Ricky" },
+    { "name": "Eugenie" },
+    { "name": "Godfrey" }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { AppComponent } from './app.component';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+	return new TranslateHttpLoader(httpClient);
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+    BrowserAnimationsModule,
+		HttpClientModule,
+    FormsModule,
+    RadioButtonModule,
+    DropdownModule,
+    CalendarModule,
+    FileUploadModule,
+    TableModule,
+    ConfirmPopupModule,
+    ButtonModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
